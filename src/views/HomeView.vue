4943314c553d09366ce573d1e258ed5ef25a4b67
simonll4 <sllamosas806@alumnos.iua.edu.ar>
2024-11-13 21:31:49 -0300

    Base de proyecto

---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-card-title>
+                Home
+              </v-card-title>
+              <v-card-text>
+                Welcome to the Home Page!
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    port: parseInt(process.env.VITE_PORT || '3001'),
+  },
+})
